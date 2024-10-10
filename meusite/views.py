f58550e30275f815b24d81464fed5a00d89cd6a4
Quelvin <quelvindev@gmail.com>
2024-10-10 17:04:37 -0300

    adicionado curriculo, paginas finalizadas

--- meusite/views.py
+++ meusite/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 def index(request):
-      
-    conext = {
+    # Aqui você pode definir a seção que deseja exibir
+    section = request.GET.get('section', 'header')  # Por padrão, mostra a seção 'default'
 
+    search_value = request.GET.get('q','').strip()
+    
+    context = {
+        'section': section,  # Passa a seção para o template
     }
-    return render(request,'meusite/index.html',conext)
+    
+    print(f'{context} valor{search_value}')
+    return render(request, 'meusite/index.html', context)

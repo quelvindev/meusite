fe8b06ba2fa49e5ac9c723057a7f061733fc4555
quelvindev <quelvindev@gmail.com>
2025-10-06 21:49:48 -0300

    alterando caminho de blog

--- core/urls.py
+++ core/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(meusite.urls)),
-    path('blog', include(blog.urls))
+    path('blog.html', include(blog.urls))
     
 ]
 
